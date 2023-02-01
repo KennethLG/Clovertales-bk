659494c2a7d3ee63da2babea80e2f92b09ce9347
KennethLG <orsonleiziaga@gmail.com>
2023-02-01 18:24:39 -0500

    Attach cors

--- src/helpers/responses.ts
+++ src/helpers/responses.ts
@@ -2,6 +2,7 @@ export class SuccessResponseCS<T> {
   body: string;
   message: string;
   statusCode: number;
+  headers: any;
 
   constructor(body: T, message: string, statusCode: number = 200) {
     this.body = JSON.stringify({
@@ -10,5 +11,9 @@ export class SuccessResponseCS<T> {
     });
     this.message = message;
     this.statusCode = statusCode;
+    this.headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    };
   }
 }
