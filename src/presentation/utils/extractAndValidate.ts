3404b3fcf2faaa22b707da38c5d12eaed669237d
KennethLG <orsonleiziaga@gmail.com>
2023-03-30 18:16:51 -0500

    Add BadRequest error when a body is not found

--- src/presentation/utils/extractAndValidate.ts
+++ src/presentation/utils/extractAndValidate.ts
@@ -1,10 +1,16 @@
 import { plainToClass } from "class-transformer";
 import { validateOrReject } from "class-validator";
+import { BadRequestError } from "./customError";
 
 export async function extractAndValidate<T extends object>(
   dtoClass: new () => T,
   data: any
 ): Promise<T> {
+
+  if (!data) {
+    throw new BadRequestError("Please provide a body");
+  }
+
   const dto = plainToClass(dtoClass, data, {
     excludeExtraneousValues: true,
   });
