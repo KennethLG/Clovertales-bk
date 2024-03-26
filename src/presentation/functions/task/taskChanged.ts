3d9a14ccf9ed0a2638e38b7d64e59b18f58ebeaf
KennethLG <orsonleiziaga@gmail.com>
2024-03-25 19:38:29 -0500

    Add multiple validations to the request object

--- src/presentation/functions/task/taskChanged.ts
+++ src/presentation/functions/task/taskChanged.ts
@@ -8,23 +8,32 @@ import { TrelloWebhookEvent } from "src/presentation/dto/taskDto";
 
 const handlerFunction: APIGatewayProxyHandlerV2 = async (event) => {
   const body = JSON.parse(event.body || "{}" as string);
-  console.log(body)
+  console.log(JSON.stringify(body, null, 2))
 
   const dto = await extractAndValidate(TrelloWebhookEvent, body);
 
-  if (body.action.type === 'updateCard' && body.action.data.listAfter) {
-    const listAfter = dto.action.data.listAfter.name;
-    if (listAfter === 'Done') {
-      const createTask = createTaskUseCaseFactory();
-      createTask.exec({
-        description: dto.action.data.card.desc,
-        title: dto.action.data.card.name
-      })
-      
-    }
+  const responseHandler = responseHandlerFactory();
+
+  if (dto.action.type !== 'updateCard') {
+    return responseHandler.clientError(`The action type is not correct. Type: ${dto.action.type}`);
   }
 
-  return responseHandlerFactory().success();
+  if (!dto.action.data.listAfter) {
+    return responseHandler.clientError(`No listAfter provided`);
+  }
+
+  const listAfter = dto.action.data.listAfter.name;
+  if (listAfter !== 'Done') {
+    return responseHandler.clientError(`The listAfter is not the desired. Name: ${listAfter}`);
+  }
+  
+  const createTask = createTaskUseCaseFactory();
+  createTask.exec({
+    description: dto.action.data.card.desc,
+    title: dto.action.data.card.name
+  })
+
+  return responseHandler.success();
 }
 
 export const handler = errorHandlerMiddleware(handlerFunction);
